9f877dc0c5814ce5123ba2ce0ba689af10fcc5a3
MustafaElshehawy <119538964+MustafaElshehawy@users.noreply.github.com>
2026-04-15 20:26:39 +0200

    Feat: Add DashboardLayout in AdminArea and Assets in root

---
+++ WMS.WebMVC/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Shared;
+
+namespace WMS.WebMVC.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Authorize(Roles = SD.SuperAdminRole)]
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
