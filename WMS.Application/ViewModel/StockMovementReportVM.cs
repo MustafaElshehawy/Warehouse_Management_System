056e494063bed2c75dac77d4865c64efc534bb4a
MustafaElshehawy <119538964+MustafaElshehawy@users.noreply.github.com>
2026-05-02 00:24:55 +0300

    Feat: Added StockMovement Report --added indext view  for filter and result --added controller to applyf filter --added ViewModel To handel data

---
+++ WMS.Application/ViewModel/StockMovementReportVM.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WMS.Core.Entities;
+using WMS.Core.Enums;
+
+namespace WMS.Application.ViewModel
+{
+    public class StockMovementReportVM
+    {
+        //parameters 
+        public ActionType? ActionType { get; set; }
+        public int? ProductId { get; set; }
+        public int? WarehouseId { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+
+        //view filter + table
+        public IEnumerable<SelectListItem> ActionTypeList { get; set; }
+        public IEnumerable<SelectListItem> productList { get; set; }
+
+        public IEnumerable<SelectListItem> WarehouseList { get; set; }
+
+
+
+        public IEnumerable<StockMovement> MovementsList { get; set; }
+
+    }
+}
---
+++ WMS.WebMVC/Areas/Admin/Controllers/ReportController.cs
@@ -0,0 +1,63 @@
+﻿tusing Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Shared;
+using WMS.Application.ViewModel;
+using WMS.Core.Entities;
+using WMS.Core.Enums;
+using WMS.Core.Repositories;
+
+namespace WMS.WebMVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.SuperAdminRole)]
+    public class ReportController : Controller
+    {
+       private readonly IUnitOfWork _unitOfWork;
+        public ReportController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        
+        [HttpGet]
+        public IActionResult Index(ActionType? ActionType, int? ProductId, int? WarehouseId ,DateTime? StartDate, DateTime? EndDate)
+        {
+            var movementsfilter = _unitOfWork.StockMovement
+                .GetAll(
+                sm => (!StartDate.HasValue || sm.CreateAt >= StartDate)
+                && (!EndDate.HasValue || sm.CreateAt <= EndDate)
+                && (!ProductId.HasValue || sm.ProductId == ProductId)
+                && (!WarehouseId.HasValue || sm.UnitId == WarehouseId)
+                && (!ActionType.HasValue || sm.ActionType == ActionType)
+                ,IncludeWord: "Product,Warehouse,Unit"
+                );
+            var movementVM = new StockMovementReportVM()
+            {
+                ActionTypeList = Enum.GetValues(typeof(ActionType)).Cast<ActionType>().Select(e => new SelectListItem {
+                    Text = e.ToString(),
+                    Value = e.ToString()
+
+                }),
+
+                productList = _unitOfWork.Product.GetAll().Select(p => new SelectListItem {
+                    Text =p.Name,
+                    Value =p.Id.ToString()
+                }),
+
+                WarehouseList =_unitOfWork.Warehouse.GetAll().Select(w=> new SelectListItem
+                {
+                    Text=w.Name,
+                    Value=w.Id.ToString()
+                }),
+                MovementsList = movementsfilter,
+
+
+
+            };
+            
+
+           
+            return View(movementVM);
+        }
+    }
+}
