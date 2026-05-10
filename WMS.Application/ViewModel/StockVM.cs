82f9be1f622f5549753a18560ca617443e6e2bf1
MustafaElshehawy <119538964+MustafaElshehawy@users.noreply.github.com>
2026-05-10 15:36:34 +0300

    feat(stock): added stock section in dashboard to view stock status -Update Dashboard layout and create Stock Index view with filtering by Warehouse.

---
+++ WMS.Application/ViewModel/StockVM.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WMS.Core.Entities;
+using WMS.Core.Enums;
+
+namespace WMS.Application.ViewModel
+{
+    public class StockVM
+    {
+        //parameters 
+        public int? WarehouseId { get; set; }
+
+
+        //view filter + table
+        public IEnumerable<SelectListItem> WarehouseList { get; set; }
+
+
+
+        public IEnumerable<Stock> StockList { get; set; }
+    }
+}
---
+++ WMS.WebMVC/Areas/Admin/Controllers/StockController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Shared;
+using WMS.Application.ViewModel;
+using WMS.Core.Repositories;
+
+namespace WMS.WebMVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.SuperAdminRole)]
+    public class StockController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public StockController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index(int? WarehouseId)
+        {
+            var stockfilter=_unitOfWork.Stock.GetAll(s=>s.WarehouseId == WarehouseId || !WarehouseId.HasValue ,IncludeWord: "Warehouse,Product,Unit");
+
+            var stockVM = new StockVM()
+            {
+                WarehouseList = _unitOfWork.Warehouse.GetAll().Select(w => new SelectListItem
+                {
+                    Text = w.Name,
+                    Value = w.Id.ToString()
+                }),
+                StockList =stockfilter,
+            };
+            return View(stockVM);
+        }
+    }
+}
